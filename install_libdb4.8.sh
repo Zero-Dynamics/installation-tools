62ab9793c8584c9db2940ebd02c0ef8f23422b66
Herbert Jr <23432027+hml-s@users.noreply.github.com>
2024-03-19 00:00:09 +0100

    Create install_libdb4.8.sh

---
+++ install_libdb4.8.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+# use: wget  && chmod +x install_libdb4.8.sh && ./install_libdb4.8.sh amd64 && rm install_libdb4.8.sh
+
+if [ "$#" -ne "1" ]; then
+	echo "usage :"
+	echo "	$0 <amd64|i386>"
+elif ! [ "$1" = "amd64" ] && ! [ "$1" = "i386" ]; then
+	echo "Bad architecture argument. First and only argument must be amd64 or i386."
+else
+	mkdir "tmplibdb" && cd "tmplibdb"
+	if [ "$1" = "amd64" ]; then
+		wget -qq "https://launchpad.net/~bitcoin/+archive/ubuntu/bitcoin/+files/libdb4.8_4.8.30-trusty1_amd64.deb"
+		wget -qq "https://launchpad.net/~bitcoin/+archive/ubuntu/bitcoin/+files/libdb4.8-dev_4.8.30-trusty1_amd64.deb"
+		wget -qq "https://launchpad.net/~bitcoin/+archive/ubuntu/bitcoin/+files/libdb4.8++_4.8.30-trusty1_amd64.deb"
+		wget -qq "https://launchpad.net/~bitcoin/+archive/ubuntu/bitcoin/+files/libdb4.8++-dev_4.8.30-trusty1_amd64.deb"
+	elif [ "$1" = "i386" ]; then
+		wget -qq "https://launchpad.net/~bitcoin/+archive/ubuntu/bitcoin/+files/libdb4.8_4.8.30-trusty1_i386.deb"
+		wget -qq "https://launchpad.net/~bitcoin/+archive/ubuntu/bitcoin/+files/libdb4.8-dev_4.8.30-trusty1_i386.deb"
+		wget -qq "https://launchpad.net/~bitcoin/+archive/ubuntu/bitcoin/+files/libdb4.8++_4.8.30-trusty1_i386.deb"
+		wget -qq "https://launchpad.net/~bitcoin/+archive/ubuntu/bitcoin/+files/libdb4.8++-dev_4.8.30-trusty1_i386.deb"
+	fi
+	dpkg -i *.deb
+	cd .. && rm -rf "tmplibdb"
+fi
