7d43b2551e008516bccb4a6aa6ce5da719beb13a
Herbert Jr. <23432027+hml-s@users.noreply.github.com>
2025-01-31 05:56:09 +0100

    Update install_wsl_cuda.sh

--- install_wsl_cuda.sh
+++ install_wsl_cuda.sh
@@ -9,18 +9,18 @@ sudo apt install -y gcc
 echo "Step 2: Installing CUDA..."
 wget https://developer.download.nvidia.com/compute/cuda/repos/wsl-ubuntu/x86_64/cuda-wsl-ubuntu.pin && \
 sudo mv cuda-wsl-ubuntu.pin /etc/apt/preferences.d/cuda-repository-pin-600 && \
-wget https://developer.download.nvidia.com/compute/cuda/12.6.3/local_installers/cuda-repo-wsl-ubuntu-12-6-local_12.6.3-1_amd64.deb && \
-sudo dpkg -i cuda-repo-wsl-ubuntu-12-6-local_12.6.3-1_amd64.deb && \
-sudo cp /var/cuda-repo-wsl-ubuntu-12-6-local/cuda-*-keyring.gpg /usr/share/keyrings/ && \
-sudo dpkg -i cuda-repo-wsl-ubuntu-12-6-local_12.6.3-1_amd64.deb && \
+wget https://developer.download.nvidia.com/compute/cuda/12.8.0/local_installers/cuda-repo-wsl-ubuntu-12-8-local_12.8.0-1_amd64.deb && \
+sudo dpkg -i cuda-repo-wsl-ubuntu-12-8-local_12.8.0-1_amd64.deb && \
+sudo cp /var/cuda-repo-wsl-ubuntu-12-8-local/cuda-*-keyring.gpg /usr/share/keyrings/ && \
+sudo dpkg -i cuda-repo-wsl-ubuntu-12-8-local_12.8.0-1_amd64.deb && \
 sudo apt-get update && \
-sudo apt-get -y install cuda-toolkit-12-6
+sudo apt-get -y install cuda-toolkit-12-8
 
 # Step 3: Set up PATH variables for CUDA
 echo "Step 3: Setting up PATH for CUDA..."
 # Check if the PATH entry already exists, then add it only if not
-grep -qxF 'export PATH=/usr/local/cuda-12.6/bin${PATH:+:${PATH}}' ~/.bashrc || \
-echo 'export PATH=/usr/local/cuda-12.6/bin${PATH:+:${PATH}}' >> ~/.bashrc
+grep -qxF 'export PATH=/usr/local/cuda-12.8/bin${PATH:+:${PATH}}' ~/.bashrc || \
+echo 'export PATH=/usr/local/cuda-12.8/bin${PATH:+:${PATH}}' >> ~/.bashrc
 # Apply the changes to the current session
 source ~/.bashrc
 
@@ -30,7 +30,7 @@ sudo apt install -y nvidia-cuda-toolkit
 
 # Step 5: Install NVIDIA Utilities for Desktop Environment
 echo "Step 5: Installing NVIDIA Utilities for Desktop Environment..."
-sudo apt install -y nvidia-utils-550 && \
+sudo apt install -y nvidia-utils-570 && \
 sudo apt install -y nvidia-settings
 
 # Check if NVIDIA utilities and CUDA installation are successful
